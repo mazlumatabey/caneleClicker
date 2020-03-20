761ced1643f7e18d080e740f163a7368512872ef
mazlum atabey <matabey@mazlum.atabey>
2020-03-20 15:25:47 +0100

    [ADD] bouton

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var adding = 10;
 function bake() {
     score += adding;
     document.getElementById("number").innerHTML = score;
+    refreshOptions();
 }
 
 var ovenPrice = 25;
@@ -15,4 +16,13 @@ function buyOven()
         document.getElementById('number').innerHTML = score;
         adding += 1;
     }
+    refreshOptions();
+}
+
+function refreshOptions()
+{
+    if (score < ovenPrice)
+        document.getElementById('ovenBtn').disabled = true;
+    else
+        document.getElementById('ovenBtn').disabled = false;
 }
